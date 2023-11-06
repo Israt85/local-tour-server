449842da86e1c0bec114b4fe6a5728943afc0596
Israt85 <isratanjuman3@gmail.com>
2023-11-06 12:24:14 +0600

    done get operation for collecting data from mongodb

--- index.js
+++ index.js
@@ -24,14 +24,30 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+
+    const database = client.db('ServiceCollection')
+    const ServiceCollection = database.collection('services')
+
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    app.get('/services', async(req,res)=>{
+        const cursor = ServiceCollection.find()
+        const result = await cursor.toArray()
+        res.send(result)
+
+    })
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
