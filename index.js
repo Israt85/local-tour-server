670cc82ae62b447da317c3f094eed42338808370
Israt85 <isratanjuman3@gmail.com>
2023-11-08 00:26:47 +0600

    added delete operation

--- index.js
+++ index.js
@@ -63,7 +63,12 @@ async function run() {
       res.send(result)
     })
 
-
+   app.delete('/service/:id', async(req,res)=>{
+    const id = req.params.id
+    const query = {_id : new ObjectId(id)}
+    const result = await ServiceCollection.deleteOne(query)
+    res.send(result)
+   })
 
 
 
